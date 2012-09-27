d32e8035654d8cac69dc0c8995b77513381b929a
Carlos Torchia <ctorchia87@gmail.com>
2012-09-26 19:48:49 -0700

    Converter for stardict decompiled tab file to readable text

---
+++ tab2html.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+#
+# Converts a stardict tab file to an HTML file
+# If you have stardict ifo/idx/dict.dz files, you first
+# need to decompile them with stardict-editor (found in stardict-tools)
+#
+# Usage: tab2html.py < dictionary.txt > dictionary.html
+#
+# Copyright (c) 2012 Carlos E. Torchia (redistribute under GPL2)
+#
+
+import sys
+import re
+
+for line in sys.stdin.readlines():
+  line = re.sub(r'^[^\t]+\t', '', line)
+  line = line.replace('\\n', '\n')
+  line = line.replace('\\\\', '\\')
+  print line
