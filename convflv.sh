240848b473b9dff940d2e2476b9981d19f6bba7d
Carlos Torchia <ctorchia87@gmail.com>
2013-03-26 23:32:18 -0700

    Shell script to convert videos in bulk

---
+++ convflv.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [[ -z "$1" ]]; then
+  echo 'Usage: '$0' <filename1.flv> [<filename2.flv> [...]]' >&2
+  exit 1
+fi
+
+while [[ -n "$1" ]]; do
+  INPUT="$1"
+  OUTPUT="${INPUT%.flv}"
+  OUTPUT="${OUTPUT%.mp4}"
+  OUTPUT="${OUTPUT%.mpeg}"
+  OUTPUT="$OUTPUT.mpg"
+
+  echo -n "$INPUT -> $OUTPUT "
+
+  # See if the filename was valid
+  if [[ "$OUTPUT" == "$INPUT.mpg" ]]; then
+    echo '[skipped]'
+    shift
+    continue
+  fi
+
+  # Convert this file
+  ffmpeg -sameq -i "$INPUT" -target ntsc-dvd "$OUTPUT" >/dev/null 2>/dev/null && (
+    echo '[success]'
+    rm "$INPUT"
+  ) || echo '[failed]'
+
+  shift
+done
